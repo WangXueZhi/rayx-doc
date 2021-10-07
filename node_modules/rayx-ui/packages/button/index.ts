eecdf20a33a039f3f3141128c3e1adcf1e41865a
Xuezhi.Wang <417787371@qq.com>
2021-10-07 17:03:08 +0800

    delete ci

--- node_modules/rayx-ui/packages/button/index.ts
+++
@@ -1,9 +0,0 @@
-import Button from './button.vue'
-import { App } from 'vue'
-
-Button.install = function (app: App) {
-  app.component(Button.name, Button)
-}
-
-// 默认导出组件
-export default Button
--- node_modules/rayx-ui/packages/scroll-bar/index.ts
+++
@@ -1,8 +0,0 @@
-import Scrollbar from './scroll-bar.vue'
-import { App } from "vue";
-
-Scrollbar.install = function (app: App) {
-  app.component(Scrollbar.name, Scrollbar)
-}
-
-export default Scrollbar
